cb42dea575eee63290521a3dd4f0dd5d8dc01a97
sunshj <sunshj317@qq.com>
2024-02-12 01:37:49 +0800

    chore: fonts dir

--- lib/vernier-caliper/actions.ts
+++ lib/vernier-caliper/actions.ts
@@ -1,11 +1,9 @@
 'use server'
-import path from 'node:path'
 import { createCanvas, registerFont } from 'canvas'
 import { delay } from '../utils'
 import { getQuestion } from '.'
 
-const resolved = path.join(process.cwd(), 'public/fonts/OpenSans.ttf')
-registerFont(resolved, { family: 'OpenSans' })
+registerFont('/fonts/OpenSans.ttf', { family: 'OpenSans' })
 
 // 主尺图片高宽
 const imgHeight = 40
