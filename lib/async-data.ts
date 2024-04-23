efc4da1f140952faee2b88198bb871ec7bd79476
sunshj <sunshj317@qq.com>
2024-04-23 18:50:45 +0800

    refactor: 重构useAsyncData函数

--- lib/async-data.ts
+++ lib/async-data.ts
@@ -1,21 +1,26 @@
 import { useEffect, useMemo, useState } from 'react'
 
 export function useAsyncData<T>(fn: () => Promise<T>) {
-  const [data, setData] = useState<T>()
+  const [data, setData] = useState<T | null>(null)
   const [error, setError] = useState(null)
-  const [refreshCount, setRefreshCount] = useState(0)
 
   const pending = useMemo(() => !data && !error, [data, error])
-  const refresh = () => setRefreshCount(p => p + 1)
+
+  const cleanUp = () => {
+    setData(null)
+    setError(null)
+  }
+
+  const refresh = () => {
+    cleanUp()
+    fn().then(setData, setError)
+  }
 
   useEffect(() => {
-    fn().then(setData).catch(setError)
+    refresh()
 
-    return () => {
-      setData(undefined)
-      setError(null)
-    }
-  }, [refreshCount])
+    return cleanUp
+  }, [])
 
   return { pending, data, error, refresh }
 }
